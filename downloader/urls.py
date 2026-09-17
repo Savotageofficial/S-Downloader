67fbcab46d75bafa3191cc805e3c717900153bac
palmhillstry <mohamedgamesar4321@gmail.com>
2026-09-17 23:37:37 +0300

    fixed not found download option for apk

--- downloader/urls.py
+++ downloader/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from downloader import views
 
 urlpatterns = [
+    path('download-android/' , views.download_android)
     path('download-app/', views.download_app, name='download_app'),
     path("" , views.home , name="downloader"),
     path("download_options/" , views.show_download_options , name="show_download_options"),
